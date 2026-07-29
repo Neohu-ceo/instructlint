66f2d246eb6b745b1587130115e0ac08b2980af8
Neohu-ceo <305103483@qq.com>
2026-07-29 14:16:02 +0800

    Prepare v0.1.3

--- src/instructlint/__init__.py
+++ src/instructlint/__init__.py
@@ -4,4 +4,4 @@ from .engine import scan_repository
 from .models import Diagnostic, InstructionFile, ScanResult
 
 __all__ = ["Diagnostic", "InstructionFile", "ScanResult", "scan_repository"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
